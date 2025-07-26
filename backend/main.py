0b9b6e983e69f364e7cec0bd9a210699a815903b
melwintjoshy <melwin465@gmail.com>
2025-07-27 00:32:14 +0530

    updated

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app = FastAPI(title="Chat with YouTube Videos Bot")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://marcus-ka7e7eosv-melwintjoshys-projects.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
